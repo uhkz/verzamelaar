9682cf79f4dfec6c02fe8e151717c0eeb7c7b252
uhkz <115628083+uhkz@users.noreply.github.com>
2023-10-02 14:12:52 +0200

    fixed link

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
   <!--NAVBAR-->
   <nav class="navbar navbar-expand-lg sticky-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">LOGO</a>
+      <a class="navbar-brand" href="../../index.php">LOGO</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -25,7 +25,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="../../index.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Dashboard</a>
