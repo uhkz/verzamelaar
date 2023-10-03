2a90e77b0daea096674695ed52bcc900e686f3d1
uhkz <115628083+uhkz@users.noreply.github.com>
2023-10-03 20:52:22 +0200

    ok

--- pages/profile.php
+++ pages/profile.php
@@ -5,16 +5,6 @@
     }
 ?>
 
-<div class="card " style="width: 18rem;">
-  <img src="../media/profile.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">
-        <?php
-        echo "<h5>$username</h5>"
-        ?>
-    </h5>
-    <p class="card-text">Profiel info.</p>
-    <a href="#" class="btn disabled">Edit</a>
-  </div>
-</div>
-
+<?php
+  echo "<h1>Profile: $username</h1>"
+?>
